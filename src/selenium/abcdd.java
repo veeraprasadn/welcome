c557ec542ba80095d39e69fa7ff085bfc9c5a720
veeny123 <gsushmareddy1116@gmail.com>
2016-09-06 12:10:36 +0530

    veeraprasad

---
+++ src/selenium/abcdd.java
@@ -0,0 +1,17 @@
+package selenium;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class abcdd {
+
+	public static void main(String[] args) 
+		{
+		WebDriver driver= new FirefoxDriver();
+		driver.get("http://google.com");
+		}
+}
